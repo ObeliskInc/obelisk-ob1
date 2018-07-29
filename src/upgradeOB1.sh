a24ab53fbfebfabec760d53be5fe89e4193e5825
David Vorick <david@nebulouslabs.com>
2018-07-28 23:26:14 -0400

    fix wrong dir in upgradeOB1.sh

--- src/upgradeOB1.sh
+++ src/upgradeOB1.sh
@@ -18,7 +18,7 @@ sshpass -p ${OB1_PASSWORD} ssh root@${OB1_NETADDRESS} << !
 # Copy over the new rootfs, replacing the existing recovery rootfs. Do this
 # first since it's larger and less important.
 echo "Beginning SCP of controlCardRootFS"
-sshpass -p ${OB1_PASSWORD} scp images/controlCardRootFS.img root@${OB1_NETADDRESS}:/tmp/newRootFS.img
+sshpass -p ${OB1_PASSWORD} scp ../images/controlCardRootFS.img root@${OB1_NETADDRESS}:/tmp/newRootFS.img
 echo "Beginning DD of controlCardRootFS"
 sshpass -p ${OB1_PASSWORD} ssh root@${OB1_NETADDRESS} << !
 	dd if=/tmp/newRootFS.img of=/dev/mtdblock4
@@ -30,7 +30,7 @@ sshpass -p ${OB1_PASSWORD} ssh root@${OB1_NETADDRESS} << !
 # second as the image is smaller and more important. We have confidence at this
 # point that there is space for it in /tmp, as we just deleted a larger file.
 echo "Beginning SCP of controlCardZImage"
-sshpass -p ${OB1_PASSWORD} scp images/controlCardZImage.img root@${OB1_NETADDRESS}:/tmp/newZImage.img
+sshpass -p ${OB1_PASSWORD} scp ../images/controlCardZImage.img root@${OB1_NETADDRESS}:/tmp/newZImage.img
 echo "Beginning DD of controlCardZImage"
 sshpass -p ${OB1_PASSWORD} ssh root@${OB1_NETADDRESS} << !
 	dd if=/tmp/newZImage.img of=/dev/mtdblock5
