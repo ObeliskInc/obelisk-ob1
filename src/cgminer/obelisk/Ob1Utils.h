5ac126a0f81df6cf11e16691eaf437a6b9b8f7bb
David Vorick <david@nebulouslabs.com>
2018-08-13 01:42:16 -0400

    merge ub1utils.h

--- src/cgminer/obelisk/Ob1Utils.h
+++ src/cgminer/obelisk/Ob1Utils.h
@@ -71,45 +71,6 @@ typedef struct ControlLoopState {
 	uint64_t goodNoncesSinceVoltageChange;
 	time_t   prevVoltageChangeTime;
 	uint64_t voltageLevelHashrates[128];
-
-	// Genetic coding for 6 children.
-	uint64_t childOneHashrate;
-	uint8_t childOneVoltageLevel;
-	int8_t childOneBiases[15];
-	uint8_t childOneDividerse[15];
-	uint64_t childTwoHashrate;
-	uint8_t childTwoVoltageLevel;
-	int8_t childTwoBiases[15];
-	uint8_t childTwoDividerse[15];
-	uint64_t childThreeHashrate;
-	uint8_t childThreeVoltageLevel;
-	int8_t childThreeBiases[15];
-	uint8_t childThreeDividerse[15];
-	uint64_t childFourHashrate;
-	uint8_t childFourVoltageLevel;
-	int8_t childFourBiases[15];
-	uint8_t childFourDividerse[15];
-	uint64_t childFiveHashrate;
-	uint8_t childFiveVoltageLevel;
-	int8_t childFiveBiases[15];
-	uint8_t childFiveDividerse[15];
-	uint64_t childSixHashrate;
-	uint8_t childSixVoltageLevel;
-	int8_t childSixBiases[15];
-	uint8_t childSixDividerse[15];
-
-	// Pre-existing control loop state:
-    uint8_t boardId;
-    double lastTemp;
-    double lastTempOnChange;
-    uint8_t currDivider;
-    int8_t currBias;
-    uint32_t ticksSinceLastChange;
-    bool lastChangeUp;
-    uint16_t printCounter;
-    uint64_t masterTickCounter;
-    double lastGHS;
-    double lastGHSStatistical;
 } ControlLoopState;
 
 // Functions for adding/subtracting bias and dividers and formatting
