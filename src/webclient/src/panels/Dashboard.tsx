fd7b11ccd4b51eaf72a5dc7694529f7fe38820c1
Eddie Wang <bonjour@eddiewang.me>
2018-08-16 19:14:22 -0400

    change to gh

--- src/webclient/src/panels/Dashboard.tsx
+++ src/webclient/src/panels/Dashboard.tsx
@@ -124,25 +124,25 @@ class Dashboard extends React.PureComponent<CombinedProps> {
       ['HASHRATE: AVG.']: (s: HashboardStatus[]) =>
         _.map(s, (h, i) => (
           <Table.Cell key={i} textAlign="center">
-            {Number(h.mhsAvg / 1000).toFixed(1)} MH/s
+            {Number(h.mhsAvg / 1000).toFixed(1)} GH/s
           </Table.Cell>
         )),
       ['HASHRATE: 1 MIN.']: (s: HashboardStatus[]) =>
         _.map(s, (h, i) => (
           <Table.Cell key={i} textAlign="center">
-            {Number(h.mhs1m / 1000).toFixed(1)} MH/s
+            {Number(h.mhs1m / 1000).toFixed(1)} GH/s
           </Table.Cell>
         )),
       ['HASHRATE: 5 MIN.']: (s: HashboardStatus[]) =>
         _.map(s, (h, i) => (
           <Table.Cell key={i} textAlign="center">
-            {Number(h.mhs5m / 1000).toFixed(1)} MH/s
+            {Number(h.mhs5m / 1000).toFixed(1)} GH/s
           </Table.Cell>
         )),
       ['HASHRATE: 15 MIN.']: (s: HashboardStatus[]) =>
         _.map(s, (h, i) => (
           <Table.Cell key={i} textAlign="center">
-            {Number(h.mhs15m / 1000).toFixed(1)} MH/s
+            {Number(h.mhs15m / 1000).toFixed(1)} GH/s
           </Table.Cell>
         )),
     }
